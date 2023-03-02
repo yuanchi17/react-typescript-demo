9820bbd473e609e5cf5cbff926350a8b811ffd67
李元琦 <yuan.chi@program.com.tw>
2023-03-02 11:37:21 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
