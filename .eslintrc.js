d7409d03db21d19891df34949235af800cfd9c5b
李元琦 <yuan.chi@program.com.tw>
2023-03-14 09:51:07 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,30 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:jest/recommended'
+  ],
   overrides: [],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect' // React version. "detect" automatically picks the version you have installed.
+    }
   },
   plugins: ['react'],
-  rules: {},
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error'
+  }
 }
